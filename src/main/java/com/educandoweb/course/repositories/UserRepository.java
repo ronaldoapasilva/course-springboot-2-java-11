a55055bcf940618b47e48b49b1b62d677b505489
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-05 16:57:53 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+// @Repository //poderia colocar essa anotação, mas não tem necessidade pq está herdando do extends JpaRepositor e que já está registrado com esse componente @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.educandoweb.course.services;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.OpAnd;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Component //essa anotação permite registrar mapear com o autowired, spring já entende
+@Service //Service é mais semanticamente especifico, mas poderia usar o component acima, essa anotação permite registrar mapear com o autowired, spring já entende
+public class UserService {
+
+	@Autowired	
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
