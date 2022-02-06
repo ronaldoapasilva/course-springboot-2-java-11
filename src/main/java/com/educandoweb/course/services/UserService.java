ce7e99c8eacdcc29f61c2f8a804d6cc0f3845642
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-06 16:41:25 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -25,7 +25,10 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
-		
+		return obj.get();		
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 }
